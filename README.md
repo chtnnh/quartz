70a9ffcbe24494671787041c5bf583e0b870c4cc
chtnnh <chtnnh@users.noreply.github.com>
2025-03-28 02:07:08 +0400

    feat: add product hunt banner

--- README.md
+++ README.md
@@ -18,6 +18,8 @@ The rest of the vault is notes about the rest of my life.
 
 © Chaitanya Mittal, 2025
 
+<a href="https://www.producthunt.com/posts/darukavana?embed=true&utm_source=badge-featured&utm_medium=badge&utm_souce=badge-darukavana" target="_blank"><img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=946919&theme=dark&t=1743112862632" alt="darukavana - chtnnh&#0039;s&#0032;digital&#0032;garden | Product Hunt" style="width: 250px; height: 54px;" width="250" height="54" /></a>
+
 ## Credits
 
 Built with Quartz V4
--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -14,8 +14,18 @@ export default ((opts?: Options) => {
     return (
       <footer class={`${displayClass ?? ""}`}>
         <p>
-          {i18n(cfg.locale).components.footer.createdWith}{" "}
-          <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
+          <a
+            href="https://www.producthunt.com/posts/darukavana?embed=true&utm_source=badge-featured&utm_medium=badge&utm_souce=badge-darukavana"
+            target="_blank"
+          >
+            <img
+              src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=946919&theme=dark&t=1743112862632"
+              alt="darukavana - chtnnh&#0039;s&#0032;digital&#0032;garden | Product Hunt"
+              style="width: 250px; height: 54px;"
+              width="250"
+              height="54"
+            />
+          </a>
         </p>
         <ul>
           {Object.entries(links).map(([text, link]) => (
